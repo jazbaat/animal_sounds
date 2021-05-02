7740bc686634fb37bbb2f6a808e204fb00520a17
Kashif Sulehry <71894219+jazbaat@users.noreply.github.com>
2021-05-02 16:06:26 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,5 @@
-function music_play(url) {
-    new Audio(url).play();
-}
+x = document.querySelector("#image1");
 
-
-document.addEventListener("keypress", function(event) {
-    if (event.keyCode == 99) {
-        new Audio('sounds/crash.mp3').play();
-        document.getElementById("demo").innerHTML = event.key + " pressed";
-        document.getElementById("char").innerHTML = event.charCode
-    } else if (event.keyCode == 107) {
-        new Audio('sounds/kick-bass.mp3').play();
-        document.getElementById("demo").innerHTML = event.key + " pressed";
-        document.getElementById("char").innerHTML = event.charCode
-    } else if (event.keyCode == 115) {
-        new Audio('sounds/snare.mp3').play();
-        document.getElementById("demo").innerHTML = event.key + " pressed";
-        document.getElementById("char").innerHTML = event.charCode
-    } else if (event.keyCode == 113) {
-        new Audio('sounds/tom-1.mp3').play();
-        document.getElementById("demo").innerHTML = event.key + " pressed";
-        document.getElementById("char").innerHTML = event.charCode
-    } else if (event.keyCode == 119) {
-        new Audio('sounds/tom-2.mp3').play();
-        document.getElementById("demo").innerHTML = event.key + " pressed";
-        document.getElementById("char").innerHTML = event.charCode
-    } else if (event.keyCode == 101) {
-        new Audio('sounds/tom-3.mp3').play();
-        document.getElementById("demo").innerHTML = event.key + " pressed";
-        document.getElementById("char").innerHTML = event.charCode
-    } else if (event.keyCode == 117) {
-        new Audio('sounds/tom-4.mp3').play();
-        document.getElementById("demo").innerHTML = event.key + " pressed";
-        document.getElementById("char").innerHTML = event.charCode
-    } else {
-        console.log(event)
-        document.getElementById("demo").innerHTML = event.key + " pressed";
-        document.getElementById("char").innerHTML = event.charCode
-    }
+x.addEventListener("click", function() {
+    alert("clicked")
 })
